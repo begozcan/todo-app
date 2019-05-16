cb13b97b0507ba7abfd7fa8c25461ecb9a120a0b
Begum Ozcan <begumozcan95@gmail.com>
2019-05-16 18:18:09 +0300

    feat(frontend): Add footer and favicon

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,15 @@ import './App.scss';
 import TodoList from './todo-list/TodoList';
 
 function App() {
-  return (
-      <div className="app-container">
-        <TodoList/>
-      </div>
-  );
+    return (
+        <div className="app-container">
+            <TodoList/>
+            <div className="footer">
+                Made with <span role="img" aria-label="love">&#x1F49B;</span> by
+                <a href="http://github.com/begozcan" target="_blank" rel="noopener noreferrer"> begozcan</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
